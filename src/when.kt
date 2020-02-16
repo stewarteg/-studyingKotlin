cf54626fb181c9b9857489c4de862ad3bcc86b0b
Stewart Gonçalves <stewartegoncalves@gmail.com>
2020-02-16 17:14:02 -0300

    k

---
+++ src/when.kt
@@ -0,0 +1,114 @@
+import java.lang.Exception
+import kotlin.math.max
+
+/*
+fun operacao (a:Int, b:Int, c:String):Int{
+when(c){
+    "soma" -> return a+b
+    "subtracao" ->{
+        return a-b
+    }
+    else-> {
+        println("invalido")
+        return 0
+    }
+}
+}
+
+fun main(){
+    operacao(a=10,b=10, c="soma")
+    //operacao(a=10,b=10, c="subtracao")
+
+}
+
+
+
+fun operacao(a:Int){
+    when{
+        // a>0 && a<100 -> {
+        a in 1..99 -> {
+            println("seculo")
+        }
+    }
+
+}
+fun main(){
+operacao(a=10)
+}
+
+
+
+// ----------- ATIVIDADES ------------
+
+fun calculo(a:Int, b:Int){
+   if (a>b){
+           println("$a é maior que $b")
+       }else if (b>a) {
+           println("$b é maior que $a")
+       } else{
+       println("valores iguais")
+   }
+}
+fun main(){
+calculo(a=20, b=20)
+}
+
+
+fun operacao(c:Int){
+if (c<10){
+    println("O valor da compra eh: ${c*3}")
+}else {
+    println("O valor da compra eh: ${c*2}")
+}
+}
+
+fun main(){
+operacao(c=11)
+}
+
+
+
+fun categoria(peso:Float){
+    when{
+        peso in 1..57 ->{
+            println("Categoria Pena")
+        }
+        peso in 57..61 ->{
+            println("Categoria Leve")
+        }
+        peso in 61..73 ->{
+            println("Categoria médio")
+        }else ->{
+            println("Categoria Pesada")
+    }
+
+    }
+}
+fun main(){
+    categoria(peso = 57.2f)
+}
+
+fun main(){
+    for (i in 0..100 step 5){
+        if (i!=80){
+        //print("$i ")
+    }}
+    for (j in 100 downTo 0){
+        print("$j ")
+    }
+}
+*/
+//fun numero(num:Int){
+
+
+fun main (){
+    //val n1:String ="hello"
+    try {
+        print("sdfn".toInt())
+    } catch (e: Exception){
+      println("ALGO NAO ESTA CERTO")
+    }
+
+
+    println("hello")
+}
